7b7f67450b4ab082788cdb0bbd9e6b014e4b1cc5
fubira <fubira@gmail.com>
2017-11-14 15:29:37 +0900

    hiの応答

--- src/module-chat-hi.js
+++ src/module-chat-hi.js
@@ -1,5 +1,19 @@
 module.exports = function(bot) {
+  // 自分が入ったときの挨拶
   bot.once('login', () => {
     bot.safechat('hi', 2000);
   });
+
+  // 最後に入ってきた人の hi に応答
+  this.last_joined_player = null;
+  bot.on('playerJoined', (player) => {
+    this.last_joined_player = player;
+  });
+
+  bot.on('chat', (username, message) => {
+    if (username === this.last_joined_player)
+      bot.safechat('hi', 2000);
+
+    this.last_joined_player = null;
+  })
 }
