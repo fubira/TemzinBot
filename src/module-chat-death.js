ecaf9943d85303e7c4b28479c38de96eb99ae5d5
fubira <fubira@gmail.com>
2017-11-14 15:25:26 +0900

    refactoring

--- src/module-chat-death.js
+++ src/module-chat-death.js
@@ -4,14 +4,16 @@ module.exports = function(bot) {
   bot.on('death', () => {
     is_dead = true;
   });
+
   bot.on('spawn', () => {
-    if (!is_dead)
-      return;
+    if (is_dead)
+    {
+      is_dead = false;
+      var messages = [ null, '死ぬかと思った', '致命傷ですんだ', 'あやうく死ぬところだった'];
+      var choice = messages[Math.floor(Math.random() * messages.length)];
 
-    var messages = [ null, '死ぬかと思った', '致命傷ですんだ', 'あやうく死ぬところだった'];
-    var choice = messages[Math.floor(Math.random() * messages.length)];
-
-    if (choice)
-      bot.safechat(choice);
+      if (choice)
+        bot.safechat(choice);
+    }
   });
 }
