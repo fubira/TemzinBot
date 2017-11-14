4a57f8f980cb4c9339ec45b98ca4a8654e01dc7f
fubira <fubira@gmail.com>
2017-11-14 16:25:34 +0900

    きよし

--- src/module-chat-kiyoshi.js
+++ src/module-chat-kiyoshi.js
@@ -1,7 +1,10 @@
 module.exports = function(bot) {
   bot.on('chat', (username, message) => {
-    if (message.match(/^(きよし|kiyoshi)$/)) {
+    if (message.match(/^きよし$/)) {
       bot.safechat('フォン');
+    }
+    if (message.match(/^kiyoshi$/)) {
+      bot.safechat('fone');
     }
   });
 }
