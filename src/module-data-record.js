156dd7e9a0a0d375a47b3b7cc86cc1a15ed99fa3
fubira <fubira@gmail.com>
2017-11-14 17:10:45 +0900

    data記憶更新

--- src/module-data-record.js
+++ src/module-data-record.js
@@ -12,6 +12,21 @@ module.exports = function(bot) {
     }
   });
 
+  function record(key, value) {
+    remove(key);
+    this.record.push({key: key, value: value});
+    jsonfile.writeFileSync(filename, this.record);
+  }
+
+  function remove(key) {
+    var new_record = this.record.filter((item, index) => {
+      if (item.key !== key) return true;
+    });
+    this.record = new_record;
+    jsonfile.writeFileSync(filename, this.record);
+}
+
+
   bot.on('chat', (username, message) => {
     // 記憶データの各キーワードとマッチ判定を行い、
     // 該当する文言があればvalueをチャットに出力する
@@ -24,8 +39,8 @@ module.exports = function(bot) {
     }
 
     // 記憶があるかどうかの確認
-    if (message.match(/(\w*)[,.。、 ]?(?:記憶|記録)(?:は？|ある？)/)) { 
-      if (this.record && record.length > 0) {
+    if (message.match(/(\w*)[,.。、 ]?記憶(?:は？|ある？)/)) {
+      if (this.record && this.record.length > 0) {
         bot.safechat(this.record[0].key + 'とか' + this.record.length + '個ぐらい');
       } else {
         bot.safechat('ないよ');
@@ -33,37 +48,29 @@ module.exports = function(bot) {
     }
 
     // 記憶の追加
-    if (message.match(/(\w*)[,.。、 ]?(?:記憶|記録|覚えて|おぼえて)[。\.\w]?([^\w=は]+)+(?:[\s=は]+)(.+)/)) {
+    if (message.match(/(\w*)[,.。、 ]?(?:記憶|保存|覚えて|おぼえて)[。\.\w]?([^\w=は ]+)+(?:[\s=は ]+)(.+)/)) {
       var target = RegExp.$1;
       var key = RegExp.$2;
       var value = RegExp.$3;
 
       if (target === bot.username) {
-        bot.log('[record] <' + key + ">:" + value);
+        bot.log('[record] target: ' + target + ', key: {' + key + '}, value: {' + value + '}');
+        record(key, value);
+
+        bot.safechat(key + 'は' + value + '、' + target + '覚えた');
       }
-
-      this.record.push({key: key, value: value});
-      jsonfile.writeFileSync(filename, this.record);
-
-      bot.safechat(key + 'は' + value + '、' + target + '覚えた');
     }
 
     // 記憶の削除
-    if (message.match(/(\w*)[,.。、 ]?(?:消去|忘れて|わすれて)[。\.\w]?([^\w=]+)/)) {
+    if (message.match(/(\w*)[,.。、 ]?(?:削除|消去|忘れて|わすれて)[。\.\w]?([^\w=]+)/)) {
       var target = RegExp.$1;
       var key = RegExp.$2;
+
       if (target === bot.username) {
-        bot.log('[delete] <' + key + ">");
+        bot.log('[record] target: ' + target + ', key: {' + key + '}');
+        remove(key, value);
+        bot.safechat(key + 'はもう消した');
       }
-
-      var new_record = this.record.filter((item, index) => {
-        if (item.key != key) return true;
-      });
-      this.record = new_record;
-
-      jsonfile.writeFileSync(filename, this.record);
-
-      bot.safechat(key + 'はもう消した');
     }
   });
 }
