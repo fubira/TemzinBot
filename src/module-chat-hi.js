676ee8ffd11b0d4836ecbbdcfd953ca460373a53
fubira <fubira@gmail.com>
2017-11-14 15:32:00 +0900

    hi

--- src/module-chat-hi.js
+++ src/module-chat-hi.js
@@ -6,13 +6,15 @@ module.exports = function(bot) {
 
   // 最後に入ってきた人の hi に応答
   this.last_joined_player = null;
+
   bot.on('playerJoined', (player) => {
     this.last_joined_player = player;
   });
 
   bot.on('chat', (username, message) => {
-    if (username === this.last_joined_player)
+    if (username !== bot.username && username === this.last_joined_player) {
       bot.safechat('hi', 2000);
+    }
 
     this.last_joined_player = null;
   })
