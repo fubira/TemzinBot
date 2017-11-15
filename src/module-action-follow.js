56797e46b7e24b631ccaefd28ce4cc10501f720e
fubira <fubira@gmail.com>
2017-11-15 18:40:18 +0900

    interest実装

--- src/module-action-follow.js
+++ src/module-action-follow.js
@@ -2,8 +2,9 @@ const delay = require('delay');
 const Vec3 = require('vec3').Vec3;
 
 module.exports = function(bot) {
-  var follow_target = undefined;
-
+  var target_entity = undefined;      // ロックされたターゲットエンティティ
+  var interest_entity = undefined;    // 興味を持っているエンティティ
+  
   function RotToVec3(pitch, yaw, rad) {
     return new Vec3(-rad * Math.cos(pitch) * Math.sin(yaw),
                      rad * Math.sin(pitch),
@@ -44,25 +45,49 @@ module.exports = function(bot) {
         if (dt < 0.25) {
           // 近接距離で顔を見て殴られたら追いかける対象として認識する
           bot.log('[bot.entitySwingArm] ' + entity.username + ' hit me!');
-          follow_target = (entity !== follow_target) ? follow_target : undefined;
+          target_entity = (entity !== target_entity) ? target_entity : undefined;
         }
       }
     }
   });
 
-  // 3秒に1回のインターバル処理
+  bot.on('entityMoved', (entity) => {
+    var distance = bot.entity.position.distanceTo(entity.position);
+
+    // 至近距離にエンティティがいる場合少し動く
+    if (distance < 0.8) {
+      var botpos = bot.entity.position;
+      var entpos = entity.position;
+      botpos.subtract(entpos);
+      bot.entity.velocity.add(botpos.scaled(60));
+    }
+    
+    // 注目中のエンティティへの対処
+    if (interest_entity === entity) {
+      // 一定よりも離れると注目をやめる
+      if (distance > 2)
+        interest_entity = undefined;
+    } else {
+      // interest_entityよりも近くに来たエンティティに興味を移す
+      if (distance < 2) {
+        if (!interest_entity || distance < bot.entity.position.distanceTo(interest_entity.position))
+          interest_entity = entity;
+      }
+    }
+  });
+
   setInterval(() => {
-    if (follow_target) {
-      var dp = bot.entity.position.subtract(follow_target.position);
+    if (target_entity) {
+      var dp = bot.entity.position.subtract(target_entity.position);
       var rot = Vec3ToRot(dp);
 
       if (Math.abs(rot.yaw - bot.yaw) > 0.05 || Math.abs(rot.pitch - bot.pitch) > 0.05) {
-        bot.log('[look] target:' + follow_target.username);
+        bot.log('[look] target:' + target_entity.username);
         bot.look(rot.yaw, 0, false, false);
       }
 
       // TODO: BehaviourTreeで実装したい
-      var dist = bot.entity.position.distanceTo(follow_target.position);
+      var dist = bot.entity.position.distanceTo(target_entity.position);
       if(dist > 2) {
         bot.setControlState('sneak', false);
         bot.setControlState("swing", false);
@@ -72,7 +97,23 @@ module.exports = function(bot) {
         bot.clearControlStates();
         bot.setControlState("swing", true);
       }
-      if (follow_target.metadata['0'] === 2) {
+
+      if (target_entity.metadata['0'] === 2) {
+        bot.setControlState('sneak', true);
+        delay(500).then(()=> {
+          bot.setControlState('sneak', false);
+        })
+      }
+    } else if (interest_entity) {
+      var dp = bot.entity.position.subtract(interest_entity.position);
+      var rot = Vec3ToRot(dp);
+
+      if (Math.abs(rot.yaw - bot.yaw) > 0.05 || Math.abs(rot.pitch - bot.pitch) > 0.05) {
+        bot.log('[look] interest:' + interest_entity.username);
+        bot.look(rot.yaw, 0, false, false);
+      }
+
+      if (interest_entity.metadata['0'] === 2) {
         bot.setControlState('sneak', true);
         delay(500).then(()=> {
           bot.setControlState('sneak', false);
@@ -81,5 +122,5 @@ module.exports = function(bot) {
     } else {
       bot.clearControlStates();
     }
-  }, 3000);
+  }, 1000);
 }
--- src/module-help.js
+++ src/module-help.js
@@ -4,8 +4,8 @@ module.exports = function(bot) {
 
     bot.safechat('/tell ' + username + ' ぼくはぼっとです。 (https://github.com/fubira/TemzinBot)');
     bot.safechat('/tell ' + username + ' === WHISPER COMMAND ===');
-    bot.safechat('/tell ' + username + ' 記憶一覧: /tell ' + bot.username + '記憶');
-    bot.safechat('/tell ' + username + ' 記憶追加: /tell ' + bot.username + '記憶|覚える|保存 <key> <value>');
-    bot.safechat('/tell ' + username + ' 記憶削除: /tell ' + bot.username + '削除|忘れる|消去 <key>');
+    bot.safechat('/tell ' + username + ' 記憶一覧: /tell ' + bot.username + ' 記憶');
+    bot.safechat('/tell ' + username + ' 記憶追加: /tell ' + bot.username + ' 記憶|覚える|保存 <key> <value>');
+    bot.safechat('/tell ' + username + ' 記憶削除: /tell ' + bot.username + ' 削除|忘れる|消去 <key>');
   });
 }
