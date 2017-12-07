04a3e5e31fc7d89681d0935b0a1320422e65a0f8
fubira <fubira@gmail.com>
2017-12-08 01:03:22 +0900

    use >

--- src/module-chat-countdown.js
+++ src/module-chat-countdown.js
@@ -4,9 +4,9 @@ module.exports = function(bot) {
 
     if (message.match(/(\w*)\W*(?:号令)/) && RegExp.$1 === bot.username) {
       bot.safechat('号令しまーす', 500);
-      bot.safechat('3', 3000);
-      bot.safechat('2', 4000);
-      bot.safechat('1', 5000);
+      bot.safechat('> 3', 3000);
+      bot.safechat('> 2', 4000);
+      bot.safechat('> 1', 5000);
       bot.safechat('> GO!', 6000);
     }
   })
