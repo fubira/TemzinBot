c6efde17f0d92e3f4742d29c45d5606aadf2af0d
fubira <fubira@gmail.com>
2021-07-20 00:14:01 +0900

    update regex

--- src/module-chat-google.js
+++ src/module-chat-google.js
@@ -2,26 +2,19 @@ module.exports = function(bot) {
   bot.on('chat', (username, message) => {
     if (username === bot.username) return;
 
-    if (message.match(/\(?(\w+)\s+(\W+)\)?/)) {
-      console.log(RegExp.$1, RegExp.$2);
-      const mode = RegExp.$1.replace(/[\(\)]/g, '');
-      const text = RegExp.$2.replace(/[\(\)]/g, '');
-      console.log({mode, text});
+    if (message.match(/(google|sksim|ggl)\s*[\(]?([^\(\)]*)[\)]?/g)) {
+        const url = `https://www.google.co.jp/search?q=${RegExp.$2}`;
+        bot.safechat(url, 1000);
+    }
 
-      if (mode.match(/(google|sksim|ggl|ごおｇｌ絵)/g)) {
-        const url = `https://www.google.co.jp/search?q=${text}`;
-        bot.safechat(url, 1000);
-      }
-  
-      if (mode.match(/(image|img|今げ)/g)) {
-        const url = `https://www.google.co.jp/search?tbm=isch&q=${text}`;
-        bot.safechat(url, 1000);
-      }
-  
-      if (mode.match(/(map|まｐ|マップ|地図)/g)) {
-        const url = `https://www.google.co.jp/maps?q=${text}`;
-        bot.safechat(url, 1000);
-      }
+    if (message.match(/(image|img)\s*[\(]?([^\(\)]*)[\)]?/g)) {
+      const url = `https://www.google.co.jp/search?tbm=isch&q=${RegExp.$2}`;
+      bot.safechat(url, 1000);
+    }
+
+    if (message.match(/(map|地図)\s*[\(]?([^\(\)]*)[\)]?/g)) {
+      const url = `https://www.google.co.jp/maps?q=${RegExp.$2}`;
+      bot.safechat(url, 1000);
     }
 
   });
