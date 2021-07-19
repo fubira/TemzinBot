97a7aa3fb1f107966c54379c8a7f06c40368a5db
fubira <fubira@gmail.com>
2021-07-20 00:26:14 +0900

    update regex

--- src/module-chat-google.js
+++ src/module-chat-google.js
@@ -2,17 +2,17 @@ module.exports = function(bot) {
   bot.on('chat', (username, message) => {
     if (username === bot.username) return;
 
-    if (message.match(/(google|sksim|ggl)\s*[\(]?([^\(\)]*)[\)]?/gi)) {
+    if (message.match(/^(google|sksim|ggl)\s*[\(]?([^\(\)]*)[\)]?/gi)) {
         const url = `https://www.google.co.jp/search?q=${RegExp.$2}`;
         bot.safechat(url, 1000);
     }
 
-    if (message.match(/(image|img)\s*[\(]?([^\(\)]*)[\)]?/gi)) {
+    if (message.match(/^(image|img)\s*[\(]?([^\(\)]*)[\)]?/gi)) {
       const url = `https://www.google.co.jp/search?tbm=isch&q=${RegExp.$2}`;
       bot.safechat(url, 1000);
     }
 
-    if (message.match(/(map|地図)\s*[\(]?([^\(\)]*)[\)]?/gi)) {
+    if (message.match(/^(map|地図)\s*[\(]?([^\(\)]*)[\)]?/gi)) {
       const url = `https://www.google.co.jp/maps?q=${RegExp.$2}`;
       bot.safechat(url, 1000);
     }
