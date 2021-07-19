9840743ae7ff9c71d4f1b6ee259131dd32f4abab
fubira <fubira@gmail.com>
2021-07-20 00:29:15 +0900

    検索内容がない場合はリンクを出さない

--- src/module-chat-google.js
+++ src/module-chat-google.js
@@ -3,18 +3,24 @@ module.exports = function(bot) {
     if (username === bot.username) return;
 
     if (message.match(/^(google|sksim|ggl)\s*[\(]?([^\(\)]*)[\)]?/gi)) {
+      if (RegExp.$2) {
         const url = `https://www.google.co.jp/search?q=${RegExp.$2}`;
         bot.safechat(url, 1000);
+      }
     }
 
     if (message.match(/^(image|img)\s*[\(]?([^\(\)]*)[\)]?/gi)) {
-      const url = `https://www.google.co.jp/search?tbm=isch&q=${RegExp.$2}`;
-      bot.safechat(url, 1000);
+      if (RegExp.$2) {
+        const url = `https://www.google.co.jp/search?tbm=isch&q=${RegExp.$2}`;
+        bot.safechat(url, 1000);
+      }
     }
 
     if (message.match(/^(map|地図)\s*[\(]?([^\(\)]*)[\)]?/gi)) {
-      const url = `https://www.google.co.jp/maps?q=${RegExp.$2}`;
-      bot.safechat(url, 1000);
+      if (RegExp.$2) {
+        const url = `https://www.google.co.jp/maps?q=${RegExp.$2}`;
+        bot.safechat(url, 1000);
+      }
     }
 
   });
