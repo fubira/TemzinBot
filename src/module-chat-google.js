447296a223f7b25d3368726c7ed008ee43f727e4
fubira <fubira@gmail.com>
2021-07-19 22:02:21 +0900

    update google

--- src/module-chat-google.js
+++ src/module-chat-google.js
@@ -2,16 +2,18 @@ module.exports = function(bot) {
   bot.on('chat', (username, message) => {
     if (username === bot.username) return;
 
-    if (message.match(/(\w*)\s+(.*)/)) {
-      const mode = RegExp.$1;
-      const text = RegExp.$2;
+    if (message.match(/\(?(\w+)\s+(\W+)\)?/)) {
+      console.log(RegExp.$1, RegExp.$2);
+      const mode = RegExp.$1.replace(/[\(\)]/g, '');
+      const text = RegExp.$2.replace(/[\(\)]/g, '');
+      console.log({mode, text});
 
-      if (mode.match(/(google|sksim|ggl)/)) {
+      if (mode.match(/(google|sksim|ggl|ごおｇｌ絵)/g)) {
         const url = `https://www.google.co.jp/search?q=${text}`;
         bot.safechat(url, 1000);
       }
   
-      if (mode.match(/(image|img)/)) {
+      if (mode.match(/(image|img|今げ)/g)) {
         const url = `https://www.google.co.jp/search?tbm=isch&q=${text}`;
         bot.safechat(url, 1000);
       }
