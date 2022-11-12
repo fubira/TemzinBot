d03e6ba64f9ac5b9153050642107f696ae1eaf76
fubira <fubira@gmail.com>
2022-11-12 18:42:15 +0900

    update

--- src/module-chat-url-repeat.js
+++ src/module-chat-url-repeat.js
@@ -2,8 +2,8 @@ module.exports = function(bot) {
   bot.on('chat', (username, message) => {
     if (bot.username === username) return;
 
-    if (message.match(/(http|https):\/\//)) {
-      bot.safechat('URLを復唱します: ' + message, 500);
+    if (message.match(/^(http|https):\/\//)) {
+      bot.safechat(`URL: ${message}` , 500);
     }
   });
 }
